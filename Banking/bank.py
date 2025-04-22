7206b18ce550db6f4287087a96f550c1ccaac383
krishna <ks08559987@gmail.com>
2025-04-22 11:14:20 +0530

    First commit - added all files

---
+++ Banking/bank.py
@@ -0,0 +1,10 @@
+# Required libraries
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report, confusion_matrix
+import joblib
